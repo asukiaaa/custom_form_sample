636f50f4ce0bb5ce6aa702629600b8fafa58da85
Asuki Kono <asukiaaa@gmail.com>
2017-10-18 12:30:12 +0900

    Show validation error messages at creating custom data

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,11 @@ module CustomFormsSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
+
     I18n.available_locales = %i[ja]
     I18n.enforce_available_locales = true
     I18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
